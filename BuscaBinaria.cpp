d5326d0ca8e10580b7a2423409927d19b693e93c
giovanni-serafim <giovannirafael0123@gmail.com>
2024-06-27 21:16:11 -0300

    BuscaBinaria.cpp

--- BuscaBinaria.cpp
+++ BuscaBinaria.cpp
@@ -2,36 +2,80 @@
   nome: BuscaBinaria.cpp
   autor: Giovanni Rafael Serafim
   data: 05/6/2024
-  descriÁ„o: informa a posiÁ„o do elemento que o usuario pedir de um vetor ordenado.
+  descri√ß√£o: informa a posi√ß√£o do elemento que o usuario pedir de um vetor ordenado.
 */
-
-
 
 #include <stdio.h>
 
-void buscabinaria(int *, int, int); //prototipando a funÁ„o
+void buscabinaria(int *, int, int); //prototipando a fun√ß√£o da busca binaria
+
+void InsertionSort (int*, int);//prototipando a fun√ß√£o do insertionSort
 
 
 int main()
 {
-	int vet[] = {-5, 1, 3, 8, 10, 12, 15, 17, 19, 24};
-	
+    int vet[10];
     int tam = sizeof(vet)/sizeof(int);
-	
+    puts("vetor de tamanho 10 ");
+    
+    for(int i=0;i<tam;i++)//preenchendo o vetor
+    {
+    	printf("digite a posicao %d do vetor: ", i+1);
+    	scanf("%d", &vet[i]);
+	}
+    
+    printf("vetor atual: ");
+
 		for (int i=0; i<tam; i++) //printando o vetor
 	{
 		printf("%d|", vet[i]);
 	}
 	
-	buscabinaria( vet,  0,  tam-1); //chamando a funÁ„o
+	InsertionSort(vet, tam);
+
+	printf("\nvetor ordenado pelo insertionSort: ");
+	
+		for (int i=0; i<tam; i++) //printando o vetor ordenado
+	{
+		printf("%d|", vet[i]);
+	}
+	
+	
+	buscabinaria( vet,  0,  tam-1); //chamando a fun√ß√£o da busca binaria
 	
 }
 
+void InsertionSort(int *lista, int tamanho) 
+{
+int i,j,aux;
+  for(i=0; i<tamanho-1; i++) //percorre o vetor
+  {
+    if(lista[i]>lista[i+1]) //se o numero atual for maior que o da frente
+	{
+     aux=lista[i+1];
+     lista[i+1]=lista[i]; lista[i]=aux; //troca
+     j=i-1; // j recebe o numero anterior
+      while(j>=0) //enquanto tiver numeros pro j voltar
+	  {
+       if(aux<lista[j]) //se o numero trocado for menor que o anterior
+	    {
+        lista[j+1]=lista[j]; lista[j]=aux;
+        }
+		else{
+         break;
+             }
+         j=j-1; // j volta mais um at√© voltar todo o vetor ou at√© o numero n√£o ser menor que ele
+         }
+      }
+   }
+}
+
+
 void buscabinaria (int *v, int ini, int fim)
 {
-	int elem;
+	int elem;//elemento pedido pelo usuario
 	int meio;
-	int achou = 0;
+	int achou = 0;//variavel para caso o elemento n√£o seja achado
 	
 	printf("\ndigite o elemento desejado: ");
 	scanf("%d", &elem);
